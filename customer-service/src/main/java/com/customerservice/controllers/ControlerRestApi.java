da49e9df812a7763a2d832cddef4143b294b902e
Nahdi <nahdibediss@yahoo.fr>
2022-06-10 14:48:40 +0200

    Configuration application.properties pour MySql

---
+++ customer-service/src/main/java/com/customerservice/controllers/ControlerRestApi.java
@@ -0,0 +1,43 @@
+package com.customerservice.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.customerservice.dto.CustomerReponseDTO;
+import com.customerservice.dto.CustomerRequestDTO;
+import com.customerservice.services.CustomerService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping(path = "/api")
+public class ControlerRestApi {
+	
+	private CustomerService customerService;
+
+	public ControlerRestApi(CustomerService customerService) {
+		super();
+		this.customerService = customerService;
+	}
+	
+	@GetMapping(path="/customers")
+	public List<CustomerReponseDTO> getAllCustomer(){
+		return customerService.listCustumers();
+	}
+	
+	@PostMapping(path="/customers")
+	public CustomerReponseDTO save (@RequestBody CustomerRequestDTO customerRequestDTO) {
+		return customerService.save(customerRequestDTO);
+	}
+	
+	@GetMapping(path="/customer/{id}")
+	public CustomerReponseDTO getCustomer (@PathVariable String id) {
+		return customerService.getCustumer(id);
+	}
+
+}
